2b3ca0fd71e043ddebe95657c764cf6293a9ba00
Voygaer2718 <voyager2718@gmail.com>
2018-01-26 13:20:01 +0800

    If user has exported ORACLE_HOME, then use env as ORACLE_HOME.

--- extract.py
+++ extract.py
@@ -29,6 +29,5 @@ def extractInfomation(command, regex, path=None, failedToExtractCallBack=None, s
         if failedToExtractCallBack is not None and "PRKF-1110" in stdout:
             return failedToExtractCallBack()
         else:
-            print(stdout)
             sys.exit(bcolors.FAIL +
                      "Failed to extract information. You may need to export ORACLE_HOME or change it in settings.py" + bcolors.ENDC)
